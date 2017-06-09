a59094553cbf35afdc1a585af603571bf33742c9
hss1994 <1070798770@qq.com>
2017-06-09 14:30:16 +0800

    v2.0.1

---
+++ static/html/config.js
@@ -0,0 +1,8 @@
+    var APIURL = "/api";
+    var SYSTEM_USERID = 'SYS_USER_CAIGO';
+    var SYSTEM_CODE ="CD-CCG000007";
+    var COMPANYCODE ="CD-CCG000007";
+//  var SHAKEURL = "http://m.test.caigo888.com"; // 分享链接
+    var SHAKEURL = "http://m.caigo888.com"; // 分享链接
+//  var SHAKEURL = "http://dev.hichengdai.com"; // 分享链接
+	var PIC_PREFIX = 'http://oondlttcb.bkt.clouddn.com/';
