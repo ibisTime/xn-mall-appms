1f8a271283950fbc2e40bc91a64062ad20584fb3
hss1994 <1070798770@qq.com>
2017-07-12 20:08:38 +0800

    v2.0.3

--- static/html/config.js
+++ static/html/config.js
@@ -3,6 +3,6 @@
     var SYSTEM_CODE ="CD-CCG000007";
     var COMPANYCODE ="CD-CCG000007";
 //  var SHAKEURL = "http://m.test.caigo888.com"; // 分享链接
-    var SHAKEURL = "http://mtest.caigo.hichengdai.com"; // 分享链接
+    var SHAKEURL = "http://m.caigo888.com"; // 分享链接
 //  var SHAKEURL = "http://dev.hichengdai.com"; // 分享链接
 	var PIC_PREFIX = 'http://oondlttcb.bkt.clouddn.com/';
